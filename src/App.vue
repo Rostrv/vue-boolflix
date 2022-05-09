c73c7cbd12b9fff1c7e95dcfecbeb221311b8590
Rostrv <trovatorosario91@gmail.com>
2022-05-09 16:11:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+    
+  </div>
+  </template>
+
+<script>
+
+
+</script>
+
+<style>
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 800px;
+  
+  
+}
+</style>
