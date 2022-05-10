057c92ad0915aaff7d37a32d9490ae81f688fc25
Rostrv <trovatorosario91@gmail.com>
2022-05-10 21:58:04 +0200

    milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,121 @@
 <template>
   <div id="app">
-    <header class="p-3 d-flex align-items-center justify-content-between">
-      <img src="@/assets/rosflix.png" alt="">
-      <div>
-        <input v-model="query" type="text" />
-      <button class="btn btn-danger ms-2" @click="callApi">Search</button>
+    <header
+      class="p-3 d-flex align-items-center justify-content-between bg-black"
+    >
+      <img src="@/assets/rosflix.png" alt="" />
+      <div class="d-flex align-items-center">
+        <input v-model="query" type="text" placeholder="cerca qualcosa..." class="p-1 bg-dark border-0 text-white border-bottom border-danger" />
+        <button class="btn btn-danger ms-3" @click="callApi">Search</button>
       </div>
     </header>
 
     <main>
-      <div class="container py-4">
-        <div class="row">
+      <div class="container-fluid py-4">
+        <div class="row justify-content-evenly">
           <div
-            class="col-2 mx-3 border border-secondary py-4 g-3"
+            class="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3 col-xxl-2 mx-2 p-1 gy-5"
             v-for="movie in movies"
             :key="movie.id"
           >
-            <h1>Movie</h1>
-            <img v-if="movie.poster_path === null" src="@/assets/not-found.png" width="200px" height="300px">
-            <img
-               v-else :src="'http://image.tmdb.org/t/p/w200/' + movie.poster_path"
-              alt=""
-            />
+            <div class="box-1">
+              <div class="box_image">
+                <img
+                  v-if="movie.poster_path === null"
+                  src="@/assets/not-found.png"
+                  class="poster"
+                />
+                <img
+                  v-else
+                  :src="'http://image.tmdb.org/t/p/w342/' + movie.poster_path"
+                  alt=""
+                  class="poster"
+                />
+              </div>
+              <div class="box_overlay">
+                <div class="overlay_text">
+                  <ul class="p-0">
+                    <h5>Titolo: {{ movie.title }}</h5>
+                    <li>Titolo originale:{{ movie.original_title }}</li>
 
-            <ul class="">
-              <h3>{{ movie.title }}</h3>
-              <h3>{{ movie.name }}</h3>
-              <li>Titolo originale:{{ movie.original_title }}</li>
-              
-              <li v-if="movie.original_language === 'en'">
-                Lingua originale <img src="@/assets/bandiera inglese.png" width="20px" />
-              </li>
-              <li v-else>Lingua originale <flag :iso="movie.original_language" /></li>
-              <Rate :length="5" :value="starsCalc(movie.vote_average)"></Rate>
-              <li>Voto {{starsCalc(movie.vote_average)}}</li>
-            </ul>
+                    <li v-if="movie.original_language === 'en'">
+                      Lingua originale:
+                      <img src="@/assets/bandiera-inglese.png" width="20px" />
+                    </li>
+                    <li v-else-if="movie.original_language === 'ja'">
+                      Lingua originale:
+                      <img
+                        src="@/assets/bandiera-giapponese.png"
+                        width="20px"
+                      />
+                    </li>
+                    <li v-else>
+                      Lingua originale: <flag :iso="movie.original_language" />
+                    </li>
+                    <Rate
+                      :length="5"
+                      :value="starsCalc(movie.vote_average)"
+                    ></Rate>
+                  </ul>
+                  <div class="description">
+                    <p>{{ movie.overview }}</p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="row">
+        <div class="row justify-content-evenly">
           <div
-            class="col-2 mx-3 border border-secondary py-4 g-3"
+            class="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3 col-xxl-2 mx-2 p-1 gy-5"
             v-for="serie in series"
             :key="serie.id"
           >
-            <h1>Series</h1>
-            <img v-if="serie.poster_path === null" src="@/assets/not-found.png" width="200px" height="300px">
-            <img
-              v-else :src="'http://image.tmdb.org/t/p/w200/' + serie.poster_path"
-              alt=""
-            />
-            <ul class="">
-              <h3>{{ serie.name }}</h3>
+            <div class="box-1">
+              <div class="box_image">
+                <img
+                  v-if="serie.poster_path === null"
+                  src="@/assets/not-found.png"
+                  class="poster"
+                />
+                <img
+                  v-else
+                  :src="'http://image.tmdb.org/t/p/w342/' + serie.poster_path"
+                  alt=""
+                  class="poster"
+                />
+              </div>
+              <div class="box_overlay">
+                <div class="overlay_text">
+                  <ul class="p-0">
+                    <h5>Titolo: {{ serie.name }}</h5>
 
-              <li>Titolo originale:{{ serie.original_name }}</li>
-              <li v-if="serie.original_language === 'en'">
-                Lingua originale <img src="@/assets/bandiera inglese.png" width="20px" />
-              </li>
-              <li v-else>Lingua <flag :iso="serie.original_language" /></li>
-              <li> Voto {{starsCalc(serie.vote_average)}} </li>
-              <Rate :length="5" :value="starsCalc(serie.vote_average)"></Rate>
-              
-              
-              
-            </ul>
+                    <li>Titolo originale:{{ serie.original_name }}</li>
+                    <li v-if="serie.original_language === 'en'">
+                      Lingua originale:
+                      <img src="@/assets/bandiera-inglese.png" width="20px" />
+                    </li>
+                    <li v-else-if="serie.original_language === 'ja'">
+                      Lingua originale:
+                      <img src="@/assets/bandiera-inglese.png" width="20px" />
+                    </li>
+                    <li v-else>
+                      Lingua <flag :iso="serie.original_language" />
+                    </li>
+
+                    <Rate
+                      :length="5"
+                      :value="starsCalc(serie.vote_average)"
+                    ></Rate>
+                    <div class="description">
+                      <p>{{ serie.overview }}</p>
+                    </div>
+                    
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -72,13 +125,13 @@
 
 <script>
 import axios from "axios";
-import Rate from "../node_modules/vue-rate/src/Rate.vue"
+import Rate from "../node_modules/vue-rate/src/Rate.vue";
 
 export default {
   name: "App",
-  components:{
-    Rate
-  },  
+  components: {
+    Rate,
+  },
   data() {
     return {
       Movies_URL:
@@ -92,13 +145,11 @@ export default {
       loading: true,
     };
   },
-  computed: {
-      
-  },
+  computed: {},
   methods: {
-    starsCalc(vote){
-            return Math.round(Number(vote) / 2);     // funzione che divide il punteggio totale / 2 -> il punteggio così sarà sulla base di max 5 stelline 
-        },
+    starsCalc(vote) {
+      return Math.round(Number(vote) / 2); // funzione che divide il punteggio totale / 2 -> il punteggio così sarà sulla base di max 5 stelline
+    },
 
     callApi() {
       this.callApiMovies(), this.callApiSeries();
@@ -143,11 +194,87 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+ul {
+  list-style: none;
+}
+
+main {
+  background-color: #0c0c0c;
+  min-height: 100vh;
+  max-height: auto;
+  
+  
+}
+
+p {
+  font-size: 15px;
+  line-height: normal;
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 800px;
+}
+
+.btn:hover{
+  box-shadow: 0px 0px 10px 10px rgba(255, 0, 0, 0.847);
+}
+
+.box-1 {
+  position: relative;
+  transition: all 0.5s ease-in;
+  height: auto;
+}
+
+.poster {
+  width: 100%;
+  height: 460px;
+}
+
+.box_image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.box_overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  visibility: none;
+  transition: 0.2s ease;
+  background-color: #393839;
+}
+
+.box-1:hover .box_overlay {
+  opacity: 1;
+  box-shadow: 0px 0px 10px 10px rgba(255, 0, 0, 0.847);
+}
+
+.description{
+overflow-y:auto;
+height: 150px;
+}
+
+.overlay_text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
 }
 </style>
