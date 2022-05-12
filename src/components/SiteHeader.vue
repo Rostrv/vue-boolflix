e8bdb268a34cceea170c3ae7add58cc1ced812e0
Rostrv <trovatorosario91@gmail.com>
2022-05-12 15:09:46 +0200

    minor changes

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,10 +1,33 @@
 <template>
-  <header
-    class="p-3 d-flex align-items-center justify-content-between bg-black"
-  >
-    <img src="@/assets/rosflix.png" alt="" />
-    <SearchBox v-model="query" @callApi="callApi" :query="query" />
-  </header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+  <div class="container-fluid">
+     <img src="@/assets/rosflix.png" alt="" class="me-5"/>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="glow" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="glow" href="#">Film</a>
+        </li>
+        <li class="nav-item">
+          <a class="glow" href="#">Serie Tv</a>
+        </li>
+        <li class="nav-item">
+          <a class="glow" href="#">Consigliati</a>
+        </li>
+        
+        
+      </ul>
+      <form class="d-flex">
+        <SearchBox v-model="query" @callApi="callApi" :query="query"/>
+      </form>
+    </div>
+  </div>
+</nav>
 </template>
 
 <script>
@@ -39,22 +62,39 @@ export default {
 
       const callApiMovies = axios.get(`${state.urlMovies}${state.query}`);
       const callApiSeries = axios.get(`${state.urlSeries}${state.query}`);
-      Promise.all([callApiMovies,callApiSeries]).then((responses) => {
-        state.movies = responses[0].data.results;
-        state.series = responses[1].data.results;
-        console.log(state.movies);
+      Promise.all([callApiMovies,callApiSeries]).then((response) => {
+        state.movies = response[0].data.results;
+        state.series = response[1].data.results;
+        /* console.log(state.movies);
         console.log('----------');
-        console.log(state.series);
+        console.log(state.series); */
       })
-      /* Salvando nello stato movies e series url
-      state.urlMovies = this.Movies_URL;
-      state.urlSseries = this.Series_URL;
-
-      // Salvo la query nello stato
-      state.query = this.query;
-      */
+     
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+a{
+  text-decoration: none;
+  color: white;
+  margin:0px 15px;
+}
+.glow {
+  font-size: 20px;
+  color: #fff;
+  text-align: center;
+  animation: glow 0.5s ease-in-out infinite alternate;
+}
+
+@-webkit-keyframes glow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 0px #e60073, 0 0 0px #e60073;
+  }
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 0px #ff4da6, 0 0 0px #ff4da6;
+}
+}
+</style>
 
